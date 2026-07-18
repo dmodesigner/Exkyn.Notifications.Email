a0e9bbc867c5d196e7bfe213b4e0b30dad4b4857
Daniel Moura <daniel.dmo@live.com>
2026-07-17 22:51:49 -0300

    Ajuste para corrigir o builder

--- Src/Exkyn.Notifications.Email/Extensions/EmailBuilderExtensions.cs
+++ Src/Exkyn.Notifications.Email/Extensions/EmailBuilderExtensions.cs
@@ -1,22 +1,22 @@
-﻿using Exkyn.Notifications.Email.Abstractions;
+﻿using Exkyn.Notifications.Abstractions;
+using Exkyn.Notifications.Email.Abstractions;
 using Exkyn.Notifications.Email.Configurations;
 using Exkyn.Notifications.Email.Providers.MailKit;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Exkyn.Notifications.Email.Extensions;
 
 public static class EmailBuilderExtensions
 {
-    //public static ExkynNotificationsBuilder AddEmail(
-    //    this IExkynNotificationsBuilder builder,
-    //    IConfiguration configuration,
-    //    string configSectionName = "EmailSettings")
-    //{
-    //    // Acessa o Services a partir do builder
-    //    builder.Services.Configure<EmailSettings>(configuration.GetSection(configSectionName));
-    //    builder.Services.AddTransient<IEmailSender, MailKitEmailSender>();
-
-    //    // Retorna o builder para permitir o ".AddWhatsApp()" depois
-    //    return builder;
-    //}
+    public static IExkynNotificationsBuilder AddEmail(
+        this IExkynNotificationsBuilder builder,
+        IConfiguration configuration,
+        string configSectionName = "EmailSettings")
+    {
+        builder.Services.Configure<EmailSettings>(configuration.GetSection(configSectionName));
+        builder.Services.AddTransient<IEmailSender, MailKitEmailSender>();
+        
+        return builder;
+    }
 }
