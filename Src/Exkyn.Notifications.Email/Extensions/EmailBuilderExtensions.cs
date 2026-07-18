fae14d5fe78f2d4b9b1a876a9dbfea0eb5027ab8
Daniel Moura de Oliveira <daniel.oliveira@gtisolution.com.br>
2026-07-17 22:20:07 -0300

    Ajustado a forma de fazer o builder do projeto para que possa ser integrado de forma mais eficiente com outros pacotes

---
+++ Src/Exkyn.Notifications.Email/Extensions/EmailBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Exkyn.Notifications.Email.Abstractions;
+using Exkyn.Notifications.Email.Configurations;
+using Exkyn.Notifications.Email.Providers.MailKit;
+using Microsoft.Extensions.Configuration;
+
+namespace Exkyn.Notifications.Email.Extensions;
+
+public static class EmailBuilderExtensions
+{
+    //public static ExkynNotificationsBuilder AddEmail(
+    //    this IExkynNotificationsBuilder builder,
+    //    IConfiguration configuration,
+    //    string configSectionName = "EmailSettings")
+    //{
+    //    // Acessa o Services a partir do builder
+    //    builder.Services.Configure<EmailSettings>(configuration.GetSection(configSectionName));
+    //    builder.Services.AddTransient<IEmailSender, MailKitEmailSender>();
+
+    //    // Retorna o builder para permitir o ".AddWhatsApp()" depois
+    //    return builder;
+    //}
+}
