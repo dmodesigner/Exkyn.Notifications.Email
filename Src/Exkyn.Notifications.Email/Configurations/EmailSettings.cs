a512b333bf8c3f6d62f83b2c6ce7a9db09313f9f
Daniel Moura <daniel.dmo@live.com>
2026-07-18 17:31:14 -0300

    Criação das constantes para PortDefault

--- Src/Exkyn.Notifications.Email/Configurations/EmailSettings.cs
+++ Src/Exkyn.Notifications.Email/Configurations/EmailSettings.cs
@@ -1,9 +1,11 @@
-﻿namespace Exkyn.Notifications.Email.Configurations;
+﻿using Exkyn.Notifications.Email.Constants;
+
+namespace Exkyn.Notifications.Email.Configurations;
 
 public class EmailSettings
 {
     public string Host { get; set; } = string.Empty;
-    public int Port { get; set; } = 587;
+    public int Port { get; set; } = PortConstant.DefaultPort;
     public string Username { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
     public bool EnableSsl { get; set; } = true;
---
+++ Src/Exkyn.Notifications.Email/Constants/PortConstant.cs
@@ -0,0 +1,6 @@
+﻿namespace Exkyn.Notifications.Email.Constants;
+
+internal class PortConstant
+{
+    public const int DefaultPort = 587;
+}
